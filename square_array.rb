4a9531384a7a13646db47d2d2530800c3f54fd5e
alice T <alicewtien@gmail.com>
2018-08-21 15:36:29 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -3,3 +3,7 @@ def square_array(array)
   array.each { |x| squared << x**2 }
   squared  #must display the new array or it will just return the original array
 end
+
+#to use .collect
+# array.collect { |x| x**2 }
+# => [1, 4, 9]
